665d0f2aa39a47c83e1ce824e4a45bd686711efd
happywhilecoding <jon@expungeus.org>
2016-03-28 15:34:47 -0700

    Added stage 2 of routes!

---
+++ app/HowTo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default React.createClass({
+	render() {
+		return <div>Expunge How-To</div>
+	}
+})
--- app/index.js
+++ app/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, hashHistory, Link } from 'react-router'
+import HowTo from './HowTo'
+import Eligible from './Eligible'
+
 
 var Home = React.createClass({
 	render: function () {
-		return <div> Home </div>
+		return (
+				<div>
+				  <h1>Home</h1>
+					<li><Link to="/howto">How-To</Link></li>
+					<li><Link to="/eligible">Eligible</Link></li>
+				</div>
+				
+		)
 	}
 });
 
 ReactDOM.render((
 	  <Router history={hashHistory}>
 			<Route path="/" component={Home}/>
+			<Route path="/howto" component={HowTo}/>
+			<Route path="/eligible" component={Eligible}/>
 		</Router>
   ),
 	document.getElementById('app')
