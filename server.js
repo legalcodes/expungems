af071cffae4bb76ce8861520f6baa1394dd7f2b1
happywhilecoding <jon@expungeus.org>
2016-04-02 15:16:26 -0700

    Changed app.use static location in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 
 var port = process.env.PORT;
 
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static('assets'));
 
 var static_path = path.join(__dirname, 'dist/');
 
