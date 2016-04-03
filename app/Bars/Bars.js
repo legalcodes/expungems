c25ba8ab22e50a66874229e63bfec933fba6e03b
happywhilecoding <jon@expungeus.org>
2016-04-02 18:46:41 -0700

    rebundled before pushing to heroku

--- app/Bars/Bars.js
+++ app/Bars/Bars.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, Link } from 'react-router';
 require("./bars.css");
+var mail = require("../Assets/web.png");
+var twitter = require("../Assets/social.png");
+var site = require("../Assets/screen.png");
 
 export default React.createClass({
 		render() {
@@ -18,15 +21,17 @@ export default React.createClass({
 										</div>
 										<div className="col-xs-2" >
 											<span>
-												<img src={require("../Assets/web.png")} />
+												<img src={ twitter }/>
 											</span>
 										</div>
 										<div className="col-xs-2" >
 											<span>
+												<img src={ site }/>
 											</span>
 										</div>
 										<div className="col-xs-2" >
 											<span>
+												<img src={ mail }/>
 											</span>
 										</div>
 									</div>
