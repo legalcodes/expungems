0c8e240166981327e5f52f2036afbdfafda02653
happywhilecoding <jon@expungeus.org>
2016-04-02 18:58:16 -0700

    Reverted to file loader

--- app/Bars/Bars.js
+++ app/Bars/Bars.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, Link } from 'react-router';
+
 require("./bars.css");
+
 var mail = require("../Assets/web.png");
 var twitter = require("../Assets/social.png");
 var site = require("../Assets/screen.png");
