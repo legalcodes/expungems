46a87b8faad358e9d03bc17f2df4f975deaad47d
happywhilecoding <jon@expungeus.org>
2016-04-02 17:30:15 -0700

    added old express code

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ var app = express();
 
 var port = process.env.PORT;
 
-var static_path = path.join(__dirname, '../..', 'dist');
+var static_path = path.join(__dirname, 'dist');
 
-app.use(express.static(path.join(static_path)))
-	.get('/', function (req, res) {
-		res.sendFile('index.html', {
-			root: static_path,
-		});
-	}).listen(process.env.PORT, function (err) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log('Listening at localhost:', process.env.PORT);
-		}
-	});
+app.use(express.static(static_path))
+  .get('/', function (req, res) {
+    res.sendFile('index.html', {
+      root: static_path,
+    });
+  }).listen(process.env.PORT || 8080, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Listening at localhost:', process.env.PORT);
+    }
+  });
