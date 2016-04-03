79febb6b1f176cac79db979ff397f24ad2c3c2fd
happywhilecoding <jon@expungeus.org>
2016-04-02 18:27:06 -0700

    eliminated 2 pictures

--- app/Bars/Bars.js
+++ app/Bars/Bars.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, Link } from 'react-router';
 require("./bars.css");
-var mail = require("../Assets/web.png");
-var twitter = require("../Assets/social.png");
-var site = require("../Assets/screen.png");
 
 export default React.createClass({
 		render() {
@@ -21,17 +18,15 @@ export default React.createClass({
 										</div>
 										<div className="col-xs-2" >
 											<span>
-												<img src={ twitter }/>
+												<img src={require("../Assets/web.png")} />
 											</span>
 										</div>
 										<div className="col-xs-2" >
 											<span>
-												<img src={ site }/>
 											</span>
 										</div>
 										<div className="col-xs-2" >
 											<span>
-												<img src={ mail }/>
 											</span>
 										</div>
 									</div>
