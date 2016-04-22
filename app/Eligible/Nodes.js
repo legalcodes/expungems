e89d114bf9afa74bd817fdbd504f7ff6de3edac3
happywhilecoding <jon@expungeus.org>
2016-04-22 07:39:45 -0700

    Fixed button navigation logic

--- app/Eligible/Nodes.js
+++ app/Eligible/Nodes.js
@@ -7,9 +7,13 @@ const graphNodes= {
 			options: [ ["Continue", "n2"], ["Do not continue", "n3"] ]
 	},
 	n2: {
-		text: "The same thing we do every night Pinky..."
+			text: "The same thing we do every night Pinky...",
+			options: [ ["2Continue", "n3"], ["2Do not continue", "n1"] ]
 	},
-		n3: "Hello"
+	n3: {
+			text: "We have reached the end",
+			options: [ ["3Back to start", "n1"], ["3Back to n2", "n2"] ]
+	},
  };
 
 export default class Nodes extends Component {
@@ -37,12 +41,14 @@ export default class Nodes extends Component {
 				console.log('New state: ', this.state);
 		}
 
-		makeButtons (nodeStr){
-				var optionsArr = graphNodes[nodeStr].options;
+		makeButtons (){
+				// find current options
+				var activeNode = this.state.activeNode;
+				var optionsArr = graphNodes[ activeNode ].options;
 				return optionsArr.map(function(tuple, index){
 						console.log("1st Element : ", tuple[0], "2nd Element ", tuple[1] );
 						return (
-										<button onClick={ this.setNewNode.bind(this, "n2") }
+										<button onClick={ this.setNewNode.bind(this, tuple[1]) }
 							     	key={index} className="btn btn-default btn-lg" type="button"
 										> { tuple[0] } </button>
 						);
@@ -53,7 +59,7 @@ export default class Nodes extends Component {
 				return (
 								<div>
 									<div> { this.state.text } </div>
-								<div> { (this.makeButtons.bind(this, "n1"))() }  </div>
+									<div> { (this.makeButtons.bind(this))() }  </div>
 								</div>
 				);
 		}
