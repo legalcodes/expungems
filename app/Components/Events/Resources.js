27777aeaebacf0f2773a139adf9caa3be7ccfeb5
legalcodes <jwtippens@gmail.com>
2016-07-03 09:33:53 -0700

    Added more styling to event container

--- app/Components/Events/Resources.js
+++ app/Components/Events/Resources.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import EventList from '../../Containers/EventList';
+import './events.css';
 
 // create Event component
 // create Events component ( that displays all events )
@@ -8,7 +9,14 @@ import EventList from '../../Containers/EventList';
 export default React.createClass({
 		render() {
 				return (
-            <EventList />
+                <div className="container-fluid event-container">
+                    <div className="row">
+                        <div className="col-md-1"></div>
+                        <div className="col-md-10"><EventList /></div>
+                        <div className="col-md-1"></div>
+                    </div>
+                </div>
+
 				);
 		}
 });
