bc968d57fd09476202c063eb0ccd5fb861c7246c
legalcodes <jwtippens@gmail.com>
2016-07-16 19:18:32 -0700

    Installed pg and added datbase.js

---
+++ models/database.js
@@ -0,0 +1,33 @@
+import pg from 'pg';
+
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/expungems-events';
+
+function query (sql, params, cb) {
+  pg.connect(DATABASE_URL, function(err, client, done){
+    if (err) {
+      done(); // release client back to pool
+      cb(err);
+      return;
+    }
+    client.query(sql, params, cb);
+  });
+}
+
+// returns event object if found, else returns undefined
+exports.findEventId = function(id, cb) {
+  var sql = `SELECT * FROM events WHERE id = $1`;
+
+  query(sql, [id], function(err, result) {
+    if(err) return cb(err);
+    cb(null, result.rows[0]);
+  });
+};
+
+// returns created user object
+
+exports.insertEvent = function(data, cb) {
+  var sql = `
+    INSERT INTO events (eventname, description)
+    VALUES ($1, $2)
+    RETURNING * `; // tells postgres to return the created user record to us
+};
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-var express = require('express');
-var path = require('path');
-var env = require('node-env-file');
-require('./webpack.config.js');
+import express from 'express';
+import path from 'path';
+import env from 'env';
+import './webpack.config.js';
+
+// var express = require('express');
+// var path = require('path');
+// var env = require('node-env-file');
+// require('./webpack.config.js');
 
 var app = express();
 
