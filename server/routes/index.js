8638397eae4f0577e9978393ba7611947fec361f
happywhilecoding <jwtippens@gmail.com>
2016-07-23 16:45:26 -0700

    Added update functionality to routes index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,17 +5,22 @@ var pg = require('pg');
 
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/expungems';
 
+function handleError(err, done, res) {
+	done();
+	console.log(err);
+	return res.status(500).json({success: false, data: err});
+};
+
+
+// READ
+
 router.get('/api/v1/expungems', function(req, res){
 
 		var results = [];
 
 		// Get Postgres client from connection pool
 		pg.connect(connectionString, function(err, client, done){
-				if(err) {
-						done();
-						console.log(err);
-						return res.status(500).json({success: false, data: err});
-				}
+			if(err) {return handleError(err, done, res);}
 
 				// SQL Query > Select Data
 				var query = client.query("SELECT * FROM events ORDER BY id ASC");
@@ -33,22 +38,18 @@ router.get('/api/v1/expungems', function(req, res){
 		});
 });
 
+// CREATE
+
 router.post('/api/v1/expungems', function(req, res) {
 
     var results = [];
 
     // Grab data from http request
-  	console.log('REQUEST FROM ROUTES INDEX JS', req.body);
     var data = {title: req.body.title, description: req.body.description};
 
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
-        // Handle connection errors
-        if(err) {
-          done();
-          console.log(err);
-          return res.status(500).json({ success: false, data: err});
-        }
+			if(err) {return handleError(err, done, res);}
 
         // SQL Query > Insert Data
         client.query("INSERT INTO events(title, description) values($1, $2)", [data.title, data.description]);
@@ -66,9 +67,43 @@ router.post('/api/v1/expungems', function(req, res) {
             done();
             return res.json(results);
         });
-
-
     });
+});
+
+// UPDATE
+
+router.put('/api/v1/expungems/:event_id', function(req, res) {
+	var results = [];
+
+	// Grab id from url params
+	var id = req.params.event_id;
+
+	// Grab data from http request
+	var data = {title: req.body.title, description: req.body.description};
+
+	console.log('DATA GRABBED FROM HTTP REQ: ////////////////// ', data);
+
+	// Get a Postgres client from connection pool
+	pg.connect(connectionString, function(err, client, done) {
+		if(err) {return handleError(err, done, res);}
+
+		// SQL Query > Update Data
+		client.query("UPDATE events SET title=($1), description=($2) WHERE id=($3)", [data.title, data.description, id]);
+
+		// SQL Query > Select Data
+		var query = client.query("SELECT * FROM events ORDER BY id ASC");
+
+		// Stream results back one row at a time
+		query.on('row', function(row){
+			results.push(row);
+		});
+
+		// After all data is returned, close connection and return results
+		query.on('end', function() {
+			done();
+			return res.json(results);
+		});
+	});
 });
 
 module.exports = router;
