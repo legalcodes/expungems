cf7d939e9b108474741d47f65d592c3e6230c100
happywhilecoding <jwtippens@gmail.com>
2016-08-01 23:42:08 -0700

    Corrected form labels and made double column

--- app/Components/EditEvents/EventForm.js
+++ app/Components/EditEvents/EventForm.js
@@ -3,13 +3,14 @@ import '../Events/events.css';
 import './editEvents.css';
 import {selectEvent} from '../../Actions/index.js';
 import { Button,
+				 Form,
 				 FormControl,
 				 FormGroup,
+				 Col,
 				 ControlLabel,
 				 HelpBlock,
 				 Checkbox,
 				 Radio
-
 			 } from 'react-bootstrap';
 
 	// <div><h4>Edit date : </h4>{this.props.e.date}</div>
@@ -29,98 +30,61 @@ function FieldGroup({ id, label, help }) {
 }
 
 function formInstance () {
-
 	return (
-		  <form>
-    <FieldGroup
-      id="formControlsText"
-      type="text"
-      label="Text"
-      placeholder="Enter text"
-    />
-    <FieldGroup
-      id="formControlsEmail"
-      type="email"
-      label="Email address"
-      placeholder="Enter email"
-    />
-    <FieldGroup
-      id="formControlsPassword"
-      label="Password"
-      type="password"
-    />
-    <FieldGroup
-      id="formControlsFile"
-      type="file"
-      label="File"
-      help="Example block-level help text here."
-    />
-​
-    <Checkbox checked readOnly>
-      Checkbox
-    </Checkbox>
-    <Radio checked readOnly>
-      Radio
-    </Radio>
-​
-    <FormGroup>
-      <Checkbox inline>
-        1
-      </Checkbox>
-      {' '}
-      <Checkbox inline>
-        2
-      </Checkbox>
-      {' '}
-      <Checkbox inline>
-        3
-      </Checkbox>
-    </FormGroup>
-    <FormGroup>
-      <Radio inline>
-        1
-      </Radio>
-      {' '}
-      <Radio inline>
-     kk   2
-      </Radio>
-      {' '}
-      <Radio inline>
-        3
-      </Radio>
-    </FormGroup>
-​
-    <FormGroup controlId="formControlsSelect">
-      <ControlLabel>Select</ControlLabel>
-      <FormControl componentClass="select" placeholder="select">
-        <option value="select">select</option>
-        <option value="other">...</option>
-      </FormControl>
-    </FormGroup>
-    <FormGroup controlId="formControlsSelectMultiple">
-      <ControlLabel>Multiple select</ControlLabel>
-      <FormControl componentClass="select" multiple>
-        <option value="select">select (multiple)</option>
-        <option value="other">...</option>
-      </FormControl>
-    </FormGroup>
+		<Form horizontal>
+			<FormGroup controlId="formHorizontalEmail">
+				<Col componentClass={ControlLabel} sm={2}>
+						Date
+				</Col>
+				<Col sm={10}>
+						<FormControl type="date" placeholder="Email" />
+				</Col>
+			</FormGroup>
+
+			<FormGroup controlId="formHorizontalEmail">
+				<Col componentClass={ControlLabel} sm={2}>
+						Title
+				</Col>
+				<Col sm={10}>
+						<FormControl type="title" placeholder="Email" />
+				</Col>
+			</FormGroup>
+
+			<FormGroup controlId="formHorizontalEmail">
+				<Col componentClass={ControlLabel} sm={2}>
+						Address
+				</Col>
+				<Col sm={10}>
+						<FormControl type="address" placeholder="Email" />
+				</Col>
+			</FormGroup>
+
 ​
     <FormGroup controlId="formControlsTextarea">
-      <ControlLabel>Textarea</ControlLabel>
-      <FormControl componentClass="textarea" placeholder="textarea" />
+			<Col componentClass={ControlLabel} sm={2}>
+				Admission Info
+			</Col>
+			<Col sm={10}>
+				<FormControl componentClass="textarea" placeholder="Email" />
+			</Col>
     </FormGroup>
-​
-    <FormGroup>
-      <ControlLabel>Static text</ControlLabel>
-      <FormControl.Static>
-        email@example.com
-      </FormControl.Static>
-    </FormGroup>
+
+
+			<FormGroup controlId="formControlsTextarea">
+				<Col componentClass={ControlLabel} sm={2}>
+						Misc Info
+				</Col>
+				<Col sm={10}>
+						<FormControl componentClass="textarea" placeholder="Email" />
+				</Col>
+			</FormGroup>
+
+
 ​
     <Button type="submit">
       Submit
     </Button>
-  </form>
+  </Form>
 	);
 }
 
