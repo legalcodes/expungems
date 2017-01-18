6ebafb93497ce496f86aea77c2743286e0dab969
legalcodes <jwtippens@gmail.com>
2017-01-17 21:27:24 -0800

    Updated events

--- app/Reducers/reducer_events.js
+++ app/Reducers/reducer_events.js
@@ -1,31 +1,22 @@
 export default function() {
 		return [
 				{
-						date: '5/19/16',
-						title: 'Expungement Legal Clinic',
-						time: '10am - 1pm',
-						address: 'Christian Services, Inc. 301 E. Second St. Hattiesburg, MS',
-						admission: 'Open by invitation to only people who are screened by calling the Mississippi Association for Justice at 601-948-8631',
-						misc: 'Sponsored by the Mississippi Access to Justice Commission, Mississippi Association for Justice, Hattiesburg Area Young Lawyers Association, Christian Services, Inc. & THRIVE',
+						date: '2/16/17',
+						title: 'Expungement Clinic',
+						time: '10am - 12pm',
+						address: 'Mississippi Center for Justice 120 Court Avenue - Indianola, MS',
+						admission: 'Services include the application and preparation of eligible STATE offenses that occurred in MISSISSIPPI for individuals who are being denied employment because of their criminal record.',
+						misc: 'Please try to bring the following documents: (1) Copy of criminal history/rap sheet/background check (2) For misdemeanor expungements, copy of ABSTRACT OF COURT RECORD related to the offense(s) to be expunged that contains your date of arrest and arresting agency (3) for felony expungements, a copy of your SENTENCING ORDER that contains your date of arrest and the arresting agency (4) For dismissals and all other matters, an ORDER OF DISMISSAL / NOLLE PROSEQUI / NO BILL / NON ADJUDICATION that contains your date of arrest and the arresting agency (5) Proof that all terms and conditions of your sentence are completed. If you have questions, please call 228-435-7284.',
             id: 0
 				},
 				{
-						date: '10/4/16',
-						title: 'Expungement Workshop',
-						time: '5:30pm',
-						address: 'Metrocenter Mall 3645 US 80, Jackson MS',
-						admission: 'Open to public',
-						misc: 'Sponsored by the Mississippi Access to Justice Commission, Mississippi Association for Justice, Capital Area Bar Association, Senator Sollie Norwood, City of Jackson & Metrocenter Mall',
+						date: '2/10/17',
+						title: 'Holly Springs Expungement Clinic',
+						time: '10am',
+						address: 'Law Offices of Roberts Wilson, P.A. 621 Highway 7 South Holly Springs, Mississippi',
+						admission: 'Call Mississippi Association for Justice by January 27th to find out if you qualify to receive help from an attorney to prepare the documents you need to clear your criminal record. Spaces are limited. Call 601-948-8631',
+						misc: '',
             id: 1
-				},
-				{
-						date: '10/28/16',
-						title: 'Expungement Legal Clinic',
-						time: '10am - 1pm',
-						address: 'Metrocenter Mall 3645 US 80, Jackson MS',
-						admission: 'Open by invitation to people who were screened at the workshop on 10/28 only',
-						misc: 'Sponsored by the Mississippi Access to Justice Commission, Mississippi Association for Justice, Capital Area Bar Association, Senator Sollie Norwood, City of Jackson & Metrocenter Mall',
-            id: 2
 				}
 		];
 }
