9fa165c5cddb4b4e42bf290624d30d371e0c5356
legalcodes <jwtippens@gmail.com>
2017-02-13 23:24:23 -0800

    Added magbar.jpg and fixed spacing in Menu.js

--- app/Components/Menu/Menu.js
+++ app/Components/Menu/Menu.js
@@ -7,40 +7,41 @@ import BoxButton from '../BoxButton/BoxButton';
 import InfoBox from '../InfoBox/InfoBox';
 import TextBox from '../TextBox/TextBox';
 import FinePrint from '../FinePrint/FinePrint';
+import magbar from '../../Assets/magbar.jpg';
 
 require("./menu.css");
 
 var frontText=<div><p> If you have a criminal record, you may have been arrested but never charged, never prosecuted, or found guilty. Still, the incident could be preventing you from achieving your goals. This problem disproportionately plagues minority communities.</p><p>Expungement is not DIY-friendly, and private legal help is expensive. ExpungeMS.org was created to address these shortcomings.</p></div>
 
 export default React.createClass({
-		render() {
-				return (
-						<div>
-								<ReactCSSTransitionGroup transitionName="example" transitionAppear={true} transitionAppearTimeout={300} transitionEnterTimeout={100} transitionLeaveTimeout={100}>
+  render() {
+    return (
+      <div>
+        <ReactCSSTransitionGroup transitionName="example" transitionAppear={true} transitionAppearTimeout={300} transitionEnterTimeout={100} transitionLeaveTimeout={100}>
+          <div className="container-fluid">
+            <div className="row">
+              <BoxButton first="EXPUNGE " third="HOW-TO" link="/howto" order="box1" firstLine="#9BA3AE" />
+              <BoxButton first="AM  I" third="ELIGIBLE ?" link="/eligible" order="box2" firstLine="#9BA3AE" />
+              <BoxButton first="FAQ & " third="RESOURCES" link="/resources" order="box3" firstLine="#2A2A2A" />
+            </div>
+          </div>
 
-								<div className="container-fluid">
-									<div className="row">
-										<BoxButton first="EXPUNGE " third="HOW-TO" link="/howto" order="box1" firstLine="#9BA3AE" />
-										<BoxButton first="AM  I" third="ELIGIBLE ?" link="/eligible" order="box2" firstLine="#9BA3AE" />
-										<BoxButton first="FAQ & " third="RESOURCES" link="/resources" order="box3" firstLine="#2A2A2A" />
-									</div>
-								</div>
+          <div className="container-fluid">
+            <div className="row">
+              <InfoBox first="77 MILLION" second="AMERICANS HAVE" third="A CRIMINAL RECORD" link="/resources"
+                firstLine="#9BA3AE" secondLine="fff" thirdLine="#650000" />
+              <TextBox grid="col-lg-7" content={frontText} />
+            </div>
+          </div>
 
-								<div className="container-fluid">
-									<div className="row">
-										<InfoBox first="77 MILLION" second="AMERICANS HAVE" third="A CRIMINAL RECORD" link="/resources"
-											firstLine="#9BA3AE" secondLine="fff" thirdLine="#650000" />
-										<TextBox grid="col-lg-7" content={frontText} />
-									</div>
-								</div>
-
-								<div className="container-fluid">
-									<div className="row">
-										<FinePrint grid="col-lg-10" />
-									</div>
-								</div>
-						</ReactCSSTransitionGroup>
-					</div>
-				);
-		}
+          <div className="container-fluid">
+            <div className="row">
+              <FinePrint grid="col-lg-10" />
+              <img src={ magbar }/>
+            </div>
+          </div>
+        </ReactCSSTransitionGroup>
+      </div>
+    );
+  }
 })
